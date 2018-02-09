19a210dfa462f4b54af7bf6228ccf5792c45d2fd
jackjacek <jacek.parszuto@gmail.com>
2018-02-09 09:40:18 +0000

    Update index.js
    Also accept an existing datadog instance

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const DD = require('node-dogstatsd').StatsD
 module.exports = function (options) {
   const host = options.host || 'localhost'
   const port = options.port || 8125
-  const datadog = new DD(host, port)
+  const datadog = options.dogstatsd || new DD(host, port)
 
   const stat = options.stat || 'node.express.router'
   const tags = options.tags || []
